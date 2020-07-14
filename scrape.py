e43c2537051598c48a4e7f5d95a383333f784e31
jamalainm <robot@inman.xyz>
2020-07-13 22:08:33 -0500

    update scrape so that text is lowercased and unidecoded

--- scrape.py
+++ scrape.py
@@ -1,5 +1,7 @@
 import bs4 as bs
 import urllib.request
+import json
+from unidecode import unidecode
 
 def get_paragraphs(url):
         """ Using beautiful soup, scrape the paragraphs from a specified page """
@@ -13,10 +15,18 @@ def get_paragraphs(url):
         for paragraph in soup.find_all('p'):
             paragraphs.append(paragraph.text)
 
-        return paragraphs
+        new_paragraphs = []
+
+        for p in paragraphs:
+            p = p.lower()
+            p = unidecode(p)
+            new_paragraphs.append(p)
+
+        return new_paragraphs
 
 if __name__ == "__main__":
-    url = 'http://www.thelatinlibrary.com/cicero/arch.shtml'
-    paragraphs = get_paragraphs(source)
-    for paragraph in paragraphs:
-        print(paragraph)
+    url = 'http://www.thelatinlibrary.com/ter.andria.html'
+    paragraphs = get_paragraphs(url)
+    filename = 'andria.json'
+    with open(filename,'w') as f:
+        json.dump(paragraphs,f)
